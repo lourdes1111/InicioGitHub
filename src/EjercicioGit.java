a77427b172760e221dd48221ccc54298bc5d5ce4
lourdes1111 <lourdesdanielamaciel@gmail.com>
2019-08-28 21:11:58 -0300

    Initial commit

---
+++ src/EjercicioGit.java
@@ -0,0 +1,8 @@
+public class EjercicioGit {
+    public static void main(String[] args) {
+        System.out.println("Linea uno");
+        System.out.println("Linea dos");
+        System.out.println("Linea tres");
+        System.out.println("Linea Cuatro");
+    }
+}
